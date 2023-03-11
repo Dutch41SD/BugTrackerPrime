52df7d2ba8c92cd87628b975ddb220b6f954f25c
Dutch41SD <81736427+Dutch41SD@users.noreply.github.com>
2023-03-11 08:15:19 -0600

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BugTrackerPrime.Data;
+using BugTrackerPrime.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BugTrackerPrime.Areas.Identity.IdentityHostingStartup))]
+namespace BugTrackerPrime.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
